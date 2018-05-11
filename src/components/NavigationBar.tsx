7e019c5f0a8fc99fdc4664c24016bd12a44a2437
Bang Chi Duong <chibangduong1993@gmail.com>
2018-05-10 21:10:57 -0700

    Use bootstrap now for navigation bar

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,29 +1,30 @@
 import * as React from 'react';
 
-import logo from '../images/logo.svg';
+// import logo from '../images/logo.svg';
 
 class NavigationBar extends React.Component {
 	public render() {
 		return (
-			<div className="NavigationBar">
+			<nav className="navbar navbar-expand-lg navbar-light bg-light">
+				<a className="navbar-brand" href="#home">BCD</a>
+				<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+					<span className="navbar-toggler-icon" />
+				</button>
 
-				<div className="navItem navName">
-					BCD <img src={logo} className="navLogo" alt="logo" />
+				<div className="collapse navbar-collapse" id="navbarSupportedContent">
+					<div className="navbar-nav mr-auto">
+						<a className="nav-item nav-link active" href="#home">Home<span className="sr-only">(current)</span></a>
+						<a className="nav-item nav-link" href="#profile">Profile</a>
+						<a className="nav-item nav-link" href="#projects">Projects</a>
+						<a className="nav-item nav-link" href="#contact">Contact</a>
+					</div>
+
+					<form className="form-inline my-2 my-lg-0">
+						<input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+						<button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+					</form>
 				</div>
-
-				<div className="navItem">
-					<a href="#footer" className="navLink"> Contact </a>
-				</div>
-
-				<div className="navItem">
-					<a href="#projects" className="navLink"> Projects </a>
-				</div>
-
-				<div className="navItem">
-					<a href="#profile" className="navLink"> Profile </a>
-				</div>
-
-			</div>
+			</nav>
 		);
 	}
 }
