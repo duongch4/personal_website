6a81c3e976b4e0896d69abc740917a8ca8deb1a7
Bang Chi Duong <chibangduong1993@gmail.com>
2018-05-15 04:58:42 -0700

    see if the icons are shown up in Contact section

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -6,8 +6,19 @@ class Contact extends React.Component {
 		return (
 			<div className="contact container">
 				<div className="row">
-					<div className="col-sm-4">
-						<img src={contactImg} alt="ContactImage" width="50px" />
+
+					<img src={contactImg} className="col-sm-4 img-fluid rounded" alt="ContactImage" />
+
+					<div className="col-sm-8 text-left">
+						<p>
+							<a href="https://www.linkedin.com/in/bang-chi-duong-6857969a/" target="_blank" rel="noopener noreferrer"><i className="fa fa-linkedin-square i-hov" aria-hidden="true" /> LinkedIn</a>
+						</p>
+						<p>
+							<a href="https://github.com/duongch4" target="_blank" rel="noopener noreferrer"><i className="fa fa-github i-hov" aria-hidden="true" /> Github</a>
+						</p>
+						<p>
+							<a href="mailto:bangchi.duong.20193@outlook.com?Subject=Hello%20there!" target="_top"><i className="fa fa-envelope i-hov" aria-hidden="true" /> bangchi.duong.20193@outlook.com</a>
+						</p>
 					</div>
 				</div>
 			</div>
