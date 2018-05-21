7e27501c321847ae3ac03d6ed07b6a8a76658e5c
Bang Chi Duong <chibangduong1993@gmail.com>
2018-05-21 09:40:35 -0700

    Add projects. Need fixing overlay

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -11,7 +11,7 @@ class Profile extends React.Component {
 						<p>
 							I come from <strong>Ho Chi Minh, Vietnam</strong>, a beautiful country with lots of delicious food <i className="fas fa-utensils" />.
 							I earned my first <i className="fas fa-flask" /> degree in <strong>Physics, Statistics and Chemistry</strong> from the <a href="https://www.utoronto.ca/" target="_blank" rel="noopener noreferrer">University of Toronto <i className="fas fa-external-link-alt" /></a>.
-							After, I completed a 1-year master's degree in <strong>Bioinformatics</strong> from the <a href="https://www.uoguelph.ca" target="_blank" rel="noopener noreferrer">University of Guelph <i className="fas fa-external-link-alt" /></a>, with a project in cancer genomic and proteomic statistical analyses.
+							After that, I completed a 1-year master's degree in <strong>Bioinformatics</strong> from the <a href="https://www.uoguelph.ca" target="_blank" rel="noopener noreferrer">University of Guelph <i className="fas fa-external-link-alt" /></a>, with a project in cancer genomic and proteomic statistical analyses.
 							During this period, I focused on expanding my <strong>machine learning</strong> theoretical knowledge and practical skill sets, mostly in <strong>Python</strong> and <strong>R</strong>.
 						</p>
 					</div>
@@ -22,7 +22,8 @@ class Profile extends React.Component {
 							I am pursuing a second bachelor's degree in <strong>Computer Science</strong> at the University of British Columbia, with 3rd-year standing.
 							I love visualising and analysing all kinds of <strong>data</strong> <i className="fas fa-chart-pie" /> <i className="fas fa-chart-line" /> <i className="fas fa-chart-bar" /> <i className="fas fa-chart-area" />; thus I usually describe myself as a <strong>data-driven</strong> person.
 							This personal website is my first attempt at using <strong>ReactJS</strong> framework, with <strong>TypeScript</strong>, custom <strong>CSS</strong> and <strong>Bootstrap</strong>.
-							I am open to opportunities as a software/web developer. <strong>Interested?</strong> <a href="mailto:bangchi.duong.20193@outlook.com?Subject=Hello%20there!" target="_top">Let me know <i className="far fa-paper-plane" /></a>.
+							I am open to opportunities as a software/web developer.
+							<br /><strong>Interested?</strong> <a href="mailto:bangchi.duong.20193@outlook.com?Subject=Hello%20there!" target="_top">Let me know <i className="far fa-paper-plane" /></a>.
 						</p>
 					</div>
 				</div>
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,12 +1,36 @@
 import * as React from 'react';
 
+import multipleClassifiers from '../images/multipleClassifiers.png';
+import objectDetection from '../images/objectDetection.jpg';
+
+import '../stylesheets/Projects.css';
+
 class Projects extends React.Component {
     public render() {
         return (
             <div className="projects container">
                 <div className="row">
-                    <div className="item">
-                        <p>blah</p>
+                    <div className="col-sm-6 item">
+                        <img src={objectDetection} className="thumb" alt="ObjectDetectionImage" />
+                        <div className="overlay container">
+                            <div className="text">
+                                <h3>Object Detection <i className="fas fa-search" /></h3>
+                                <p><strong>Detect cars within images</strong></p>
+                                <p>Python, OpenCV, Scikit-learn</p>
+                                <p><a href="https://github.com/duongch4/objectDetection" target="_blank" rel="noopener noreferrer">Repo <i className="fab fa-github" aria-hidden="true" /></a></p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-sm-6 item">
+                        <img src={multipleClassifiers} className="thumb" alt="MultipleClassifersImage" />
+                        <div className="overlay">
+                            <div className="text">
+                                <h3>Multiple Classifiers<i className="fas fa-search" /></h3>
+                                <p><strong>Classify benign/malignant cancer</strong></p>
+                                <p>R</p>
+                                <p><a href="https://github.com/duongch4/mammographic-classification" target="_blank" rel="noopener noreferrer">Repo <i className="fab fa-github" aria-hidden="true" /></a></p>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
