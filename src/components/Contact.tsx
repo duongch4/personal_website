d0f9662455c9902e7b67273638859fc043075f2a
Bang Chi Duong <chibangduong1993@gmail.com>
2018-09-10 01:47:59 -0400

    Add back link for resume

---
+++ src/components/Contact.tsx
@@ -0,0 +1,38 @@
+import * as React from 'react';
+
+import contactImg from '../images/contactImg.png';
+import '../stylesheets/Contact.css';
+
+class Contact extends React.Component {
+	public render() {
+		return (
+			<div id="contact" className="container-fluid">
+
+				<div className="row header"><h2>Contact</h2></div>
+
+				<div className="row">
+
+					<img src={contactImg} className="contact-img col-sm-6 img-fluid" alt="ContactImage" />
+
+					<div className="col-sm-6 text-center">
+						<h3>Let's Talk</h3>
+						<p>I'm open to new opportunities and projects.</p>
+
+						<hr />
+
+						<p><a href="https://1drv.ms/b/s!AlPEhSAAUWCmhdglUEuu_XTvQxlrAw" target="_blank" rel="noopener noreferrer">
+							<button type="button" className="btn btn-light">Resume <i className="fas fa-file-pdf pdf-icon" aria-hidden="true" /></button>
+						</a></p>
+
+						<p><a href="mailto:bangchi.duong.20193@outlook.com?Subject=Hello%20there!" target="_top">
+							<button type="button" className="btn btn-light">Send Email <i className="fas fa-envelope-square social-icons" aria-hidden="true" /></button>
+						</a></p>	
+					</div>
+
+				</div>	
+			</div>
+		);
+	}
+}
+
+export default Contact;
---
+++ src/components/Home.tsx
@@ -0,0 +1,50 @@
+import * as React from 'react';
+import homeImg from '../images/homeImg.jpg';
+import '../stylesheets/Home.css';
+// import GoogleMapContainer from './GoogleMapContainer';
+
+class Home extends React.Component {
+	public render() {
+		return (
+			<div id="home" className="container-fluid">
+				<div className="row">
+
+					<div className="col-md-4 img-sect d-none d-md-block"><img src={homeImg} className="home-img img-fluid" alt="HomeImage" /></div>
+
+					<div className="col-md-8 text-center">
+						<h1>Hi, I'm Bang Chi Duong!</h1>
+						<hr />
+						<p>
+							I'm an aspiring <strong>full-stack web developer</strong> and <strong>data scientist</strong> based in Vancouver.
+						</p>
+
+						{/* <div className="col-sm-12"><GoogleMapContainer /></div> */}
+
+						<h6>Connect with me</h6>
+
+						<div className="social-icons-sect">
+						<a href="https://github.com/duongch4" target="_blank" rel="noopener noreferrer"><i className="fab fa-github-square social-icons" aria-hidden="true" /></a>
+						<a href="mailto:bangchi.duong.20193@outlook.com?Subject=Hello%20there!" target="_top"><i className="fas fa-envelope-square social-icons" aria-hidden="true" /></a>
+						<a href="https://www.linkedin.com/in/bang-chi-duong-6857969a/" target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin social-icons" aria-hidden="true" /></a>
+						</div>
+
+						<div>
+						<a href="https://1drv.ms/b/s!AlPEhSAAUWCmhdglUEuu_XTvQxlrAw" target="_blank" rel="noopener noreferrer">
+							<button className="resume-button">
+								Resume <i className="far fa-file-pdf pdf-icon" aria-hidden="true" />
+							</button>
+						</a>
+						</div>
+					</div>
+
+
+
+				</div>
+			</div>
+		);
+	}
+}
+
+export default Home;
+
+// <img src={profileImg} className="main-img col-sm-5 img-fluid rounded-circle d-none d-sm-block" alt="HomeImage" />
