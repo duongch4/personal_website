93e7a4c843450f5687380f694349e85d30e4bf79
Bang Chi Duong <chibangduong1993@gmail.com>
2018-09-16 16:46:42 -0400

    Change Linkedin link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ class Footer extends React.Component {
 		return (
 			<nav className="footer navbar-dark bg-dark static-bottom text-center">
 				<div className="social-icons">
-					<a href="https://www.linkedin.com/in/bang-chi-duong-6857969a/" target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin social-icons" aria-hidden="true" /></a>
+					<a href="https://www.linkedin.com/in/bang-chi-duong/" target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin social-icons" aria-hidden="true" /></a>
 					<a href="https://github.com/duongch4" target="_blank" rel="noopener noreferrer"><i className="fab fa-github-square social-icons" aria-hidden="true" /></a>
 					<a href="mailto:bangchi.duong.20193@outlook.com?Subject=Hello%20there!" target="_top"><i className="fas fa-envelope-square social-icons" aria-hidden="true" /></a>
 				</div>
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -25,7 +25,7 @@ class Home extends React.Component {
 						<div className="social-icons-sect">
 						<a href="https://github.com/duongch4" target="_blank" rel="noopener noreferrer"><i className="fab fa-github-square social-icons" aria-hidden="true" /></a>
 						<a href="mailto:bangchi.duong.20193@outlook.com?Subject=Hello%20there!" target="_top"><i className="fas fa-envelope-square social-icons" aria-hidden="true" /></a>
-						<a href="https://www.linkedin.com/in/bang-chi-duong-6857969a/" target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin social-icons" aria-hidden="true" /></a>
+						<a href="https://www.linkedin.com/in/bang-chi-duong/" target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin social-icons" aria-hidden="true" /></a>
 						</div>
 
 						<div>
