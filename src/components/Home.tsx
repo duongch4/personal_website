43d76faf6b288936510ac6e791fa7cd3de1db130
Bang Chi Duong <chibangduong1993@gmail.com>
2018-09-16 19:29:57 -0400

    Make links end with '/'

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -23,13 +23,13 @@ class Home extends React.Component {
 						<h6>Connect with me</h6>
 
 						<div className="social-icons-sect">
-						<a href="https://github.com/duongch4" target="_blank" rel="noopener noreferrer"><i className="fab fa-github-square social-icons" aria-hidden="true" /></a>
+						<a href="https://github.com/duongch4/" target="_blank" rel="noopener noreferrer"><i className="fab fa-github-square social-icons" aria-hidden="true" /></a>
 						<a href="mailto:bangchi.duong.20193@outlook.com?Subject=Hello%20there!" target="_top"><i className="fas fa-envelope-square social-icons" aria-hidden="true" /></a>
 						<a href="https://www.linkedin.com/in/bang-chi-duong/" target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin social-icons" aria-hidden="true" /></a>
 						</div>
 
 						<div>
-						<a href="https://1drv.ms/b/s!AlPEhSAAUWCmhd5wSLA_HvivQNhaxw" target="_blank" rel="noopener noreferrer">
+						<a href="https://1drv.ms/b/s!AlPEhSAAUWCmhd5wSLA_HvivQNhaxw/" target="_blank" rel="noopener noreferrer">
 							<button className="resume-button">
 								Resume <i className="far fa-file-pdf pdf-icon" aria-hidden="true" />
 							</button>
