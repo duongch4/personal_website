311950ed6050229e3cef7f18a62a35297ecdeeb4
Bang Chi Duong <chibangduong1993@gmail.com>
2019-12-16 13:35:02 -0800

    Refactor Projects; Added gameplay video

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,22 +1,10 @@
 import * as React from "react";
-
-import * as bioinformatics from "../assets/png/bioinformatics.png";
-import * as imageCompression from "../assets/png/imageCompression.png";
-import * as deepCloth from "../assets/png/deepCloth.png";
-import * as movieApp from "../assets/png/movieApp.png";
-import * as multipleClassifiers from "../assets/png/multipleClassifiers.png";
-import * as objectDetection from "../assets/jpg/objectDetection.jpg";
-import * as game from "../assets/png/CaptureTheCastle.png";
-
-import * as deepClothPdf from "../assets/pdf/DeepClothSCA.pdf";
-import * as deepClothVid from "../assets/mp4/DeepClothSCA.mp4";
-
-import * as gameExe from "../assets/7z/Capture-The-Castle-x64-Release.7z";
+import { projectsInfo, ProjectLinks, ProjectInfo, ProjectsInfo } from "./ProjectsInfo";
 
 export class Projects extends React.Component<any, any> {
 
     public render() {
-        const projects: React.ReactElement[] = this.getProjects();
+        const projects: React.ReactElement[] = this.setProjects();
         return (
             <div id="projects" className="container-fluid">
                 <div className="row header"><h2>Projects</h2></div>
@@ -27,73 +15,91 @@ export class Projects extends React.Component<any, any> {
         );
     }
 
+    private setProjects(): React.ReactElement[] {
+        const projects: React.ReactElement[] = [];
+        for (const id in projectsInfo) {
+            projects.push(this.setProject(projectsInfo[id]));
+        }
+        return projects;
+    }
+
     private renderProjects = (projects: React.ReactElement[]): React.ReactElement[] => {
-        const result = [];
+        const result: React.ReactElement[] = [];
         for (let i = 0; i < projects.length; i += 3) {
-            result.push(this.renderOneRow(projects[i], projects[i + 1], projects[i + 2]));
+            result.push(<div className="row">{projects[i]}{projects[i + 1]}{projects[i + 2]}</div>);
         }
         return result;
     }
 
-    private renderProject = (
-        imgSrc: any, altSring: string, title: string, description: string[],
-        languages: string[], repoLink: string, icon: string, webAppLink: string
-    ): React.ReactElement => {
+    private setProject = (project: ProjectInfo): React.ReactElement => {
         return (
             <div className="col-lg-4 mb-4">
                 <div className="item col-md-12">
-                    <img src={imgSrc} className="thumb-img" alt={altSring} />
+                    <img src={project.image} className="thumb-img" alt={project.altStr} />
                     <div className="overlay">
                         <div className="text">
-                            {
-                                webAppLink ?
-                                    <p><a href={webAppLink} target="blank" rel="noopener noreferer">
-                                        {this.renderSpanList(description)}
-                                    </a></p>
-                                    :
-                                    <p>{this.renderSpanList(description)}</p>
-                            }
-                            <p>{this.renderSpanList(languages)}</p>
-                            <p><a href={repoLink} target="_blank" rel="noopener noreferrer">
-                                Repo <i className="fab fa-github" aria-hidden="true" />
-                            </a></p>
+                            {this.setDescription(project.links, project.description)}
+                            <p>{this.setSpanList(project.languages)}</p>
+                            {this.setLinks(project.links)}
                         </div>
                     </div>
                 </div>
-                <h5><strong>{title}</strong> <i className={icon} /></h5>
+                <h5><strong>{project.title}</strong> <i className={project.icon} /></h5>
             </div>
         );
     }
 
-    private renderResearch = (
-        imgSrc: any, altSring: string, title: string, description: string[],
-        languages: string[], icon: string, acmLink: string, pdfLink: any, videoLink: any
-    ): React.ReactElement => {
+    private setDescription(links: ProjectLinks, description: string[]): React.ReactElement {
         return (
-            <div className="col-lg-4 mb-4">
-                <div className="item col-md-12">
-                    <img src={imgSrc} className="thumb-img" alt={altSring} />
-                    <div className="overlay">
-                        <div className="text">
-                            <p><a href={acmLink} target="blank" rel="noopener noreferer">
-                                {this.renderSpanList(description)}
-                            </a></p>
-                            <p>{this.renderSpanList(languages)}</p>
-                            <p><a href={pdfLink} target="_blank" rel="noopener noreferrer">
-                                Paper <i className="fas fa-file-pdf" aria-hidden="true" />
-                            </a></p>
-                            <p><a href={videoLink} target="_blank" rel="noopener noreferrer">
-                                Video <i className="fas fa-video" aria-hidden="true" />
-                            </a></p>
-                        </div>
-                    </div>
-                </div>
-                <h5><strong>{title}</strong> <i className={icon} /></h5>
+            links.description ?
+                <p><a href={links.description} target="blank" rel="noopener noreferer">
+                    {this.setSpanList(description)}
+                </a></p>
+                :
+                <p>{this.setSpanList(description)}</p>
+        );
+    }
+
+    private setLinks = (links: ProjectLinks): React.ReactElement => {
+        return (
+            <div>
+                {
+                    links.repo ?
+                        <p><a href={links.repo.link} target="_blank" rel="noopener noreferrer">
+                            {links.repo.id} <i className="fab fa-github" aria-hidden="true" />
+                        </a></p>
+                        :
+                        undefined
+                }
+                {
+                    links.pdf ?
+                        <p><a href={links.pdf.link} target="_blank" rel="noopener noreferrer">
+                            {links.pdf.id} <i className="fas fa-file-pdf" aria-hidden="true" />
+                        </a></p>
+                        :
+                        undefined
+                }
+                {
+                    links.video ?
+                        <p><a href={links.video.link} target="_blank" rel="noopener noreferrer">
+                            {links.video.id} <i className="fas fa-video" aria-hidden="true" />
+                        </a></p>
+                        :
+                        undefined
+                }
+                {
+                    links.zip ?
+                        <p><a href={links.zip.link} target="_blank" rel="noopener noreferrer">
+                            {links.zip.id} <i className="fas fa-file-archive" aria-hidden="true" />
+                        </a></p>
+                        :
+                        undefined
+                }
             </div>
         );
     }
 
-    private renderSpanList = (spanList: string[]): React.ReactElement[] => {
+    private setSpanList = (spanList: string[]): React.ReactElement[] => {
         return (
             spanList.map((value) => {
                 return (
@@ -101,52 +107,5 @@ export class Projects extends React.Component<any, any> {
                 );
             })
         );
-    }
-
-    private renderOneRow = (
-        proj1: React.ReactElement, proj2: React.ReactElement, proj3: React.ReactElement
-    ): React.ReactElement => {
-        return (<div className="row">{proj1}{proj2}{proj3}</div>);
-    }
-
-    private getProjects(): React.ReactElement[] {
-        return [
-            this.renderProject(
-                game, "CaptureTheCastle", "Capture The Castle",
-                ["2D Game"], ["C++, OpenGL"],
-                gameExe, "fas fa-gamepad", undefined
-            ),
-            this.renderResearch(
-                deepCloth, "DeepClothImage", "Subspace Neural Physics",
-                ["Fast Data-Driven", "Interactive Simulation"], ["Python, C++"],
-                "fas fa-gamepad", "https://dl.acm.org/citation.cfm?id=3340245", deepClothPdf, deepClothVid
-            ),
-            this.renderProject(
-                movieApp, "MovieAppImage", "Browse/Review Movies",
-                ["Browse/Review Movies"], ["MongoDB, Express", "React, NodeJS"],
-                "https://github.com/duongch4/mern-passport-dev/", "fas fa-search",
-                "https://cs490-project-movie.herokuapp.com/"
-            ),
-            this.renderProject(
-                objectDetection, "ObjectDetectionImage", "Object Detection",
-                ["Detect cars within images"], ["Python, OpenCV"],
-                "https://github.com/duongch4/objectDetection/", "fas fa-search", undefined
-            ),
-            this.renderProject(
-                imageCompression, "LossyImageCompressionImage", "Lossy Image Compression",
-                ["Compress images"], ["C++"],
-                "https://github.com/duongch4/lossy-image-compression/", "far fa-image fa-lg", undefined
-            ),
-            this.renderProject(
-                bioinformatics, "BioinformaticsImage", "Bioinformatics",
-                ["Analyse cancer genomics"], ["R, edgeR", "ggplot2, MSqRob, GSEA"],
-                "https://github.com/duongch4/genomics-proteomics-analysis/", "fas fa-dna", undefined
-            ),
-            this.renderProject(
-                multipleClassifiers, "MultipleClassifersImage", "Multiple Classifiers",
-                ["Classify cancer", "benign/malignant"], ["R, ggplot2"],
-                "https://github.com/duongch4/mammographic-classification/", "fas fa-dna", undefined
-            )
-        ];
     }
 }
---
+++ src/components/ProjectsInfo.tsx
@@ -0,0 +1,116 @@
+import * as bioinformaticsImg from "../assets/png/bioinformatics.png";
+import * as imageCompressionImg from "../assets/png/imageCompression.png";
+import * as deepClothImg from "../assets/png/deepCloth.png";
+import * as movieAppImg from "../assets/png/movieApp.png";
+import * as multipleClassifiersImg from "../assets/png/multipleClassifiers.png";
+import * as objectDetectionImg from "../assets/jpg/objectDetection.jpg";
+import * as gameImg from "../assets/png/CaptureTheCastle.png";
+
+import * as deepClothPdf from "../assets/pdf/DeepClothSCA.pdf";
+import * as deepClothVid from "../assets/mp4/DeepClothSCA.mp4";
+
+import * as gameExeZip from "../assets/7z/CaptureTheCastle-x64-Release.7z";
+import * as gameVideo from "../assets/mp4/CaptureTheCastle.mp4";
+
+export type LinkName = {
+    id: string,
+    link: string
+};
+
+export type ProjectLinks = {
+    description?: string,
+    repo?: LinkName,
+    pdf?: LinkName,
+    video?: LinkName,
+    zip?: LinkName
+};
+
+export type ProjectInfo = {
+    image: string,
+    altStr: string,
+    title: string,
+    description: string[],
+    languages: string[],
+    icon: string,
+    links: ProjectLinks
+};
+
+export type ProjectsInfo = {
+    [id: string]: ProjectInfo
+}
+
+export const projectsInfo: ProjectsInfo = {
+    game: {
+        image: gameImg,
+        altStr: "Capture The Castle Image",
+        title: "Capture The Castle",
+        description: ["2D Game"],
+        languages: ["C++, OpenGL"],
+        icon: "fas fa-gamepad",
+        links: {
+            zip: { id: "Executable Zip", link: gameExeZip },
+            video: { id: "Gameplay", link: gameVideo }
+        }
+    },
+    research: {
+        image: deepClothImg,
+        altStr: "Deep Cloth Image",
+        title: "Subspace Neural Physics",
+        description: ["Fast Data-Driven", "Interactive Simulation"],
+        languages: ["Python, C++"],
+        icon: "fas fa-gamepad",
+        links: {
+            description: "https://dl.acm.org/citation.cfm?id=3340245",
+            pdf: { id: "Paper", link: deepClothPdf },
+            video: { id: "Video", link: deepClothVid }
+        }
+    },
+    movieApp: {
+        image: movieAppImg,
+        altStr: "Movie App Image",
+        title: "Browse/Review Movies",
+        description: ["Browse/Review Movies"],
+        languages: ["MongoDB, Express", "React, NodeJS"],
+        icon: "fas fa-search",
+        links: {
+            description: "https://cs490-project-movie.herokuapp.com/",
+            repo: { id: "Repo", link: "https://github.com/duongch4/mern-passport-dev/" }
+        }
+    },
+    objectDetection: {
+        image: objectDetectionImg,
+        altStr: "Object Detection Image",
+        title: "Object Detection",
+        description: ["Detect cars within images"],
+        languages: ["Python, OpenCV"],
+        icon: "fas fa-search",
+        links: { repo: { id: "Repo", link: "https://github.com/duongch4/objectDetection/" } }
+    },
+    imageCompression: {
+        image: imageCompressionImg,
+        altStr: "Lossy Image Compression Image",
+        title: "Lossy Image Compression",
+        description: ["Compress images"],
+        languages: ["C++"],
+        icon: "far fa-image fa-lg",
+        links: { repo: { id: "Repo", link: "https://github.com/duongch4/lossy-image-compression/" } }
+    },
+    bioinformatics: {
+        image: bioinformaticsImg,
+        altStr: "Bioinformatics Image",
+        title: "Bioinformatics",
+        description: ["Analyse cancer genomics"],
+        languages: ["R, edgeR", "ggplot2, MSqRob, GSEA"],
+        icon: "fas fa-dna",
+        links: { repo: { id: "Repo", link: "https://github.com/duongch4/genomics-proteomics-analysis/" } }
+    },
+    multipleClassifiers: {
+        image: multipleClassifiersImg,
+        altStr: "Multiple Classifers Image",
+        title: "Multiple Classifiers",
+        description: ["Classify cancer", "benign/malignant"],
+        languages: ["R, ggplot2"],
+        icon: "fas fa-dna",
+        links: { repo: { id: "Repo", link: "https://github.com/duongch4/mammographic-classification/" } }
+    },
+};
